78fdd1884491355cb5fe6894c13a7c6cfaa49014
ignatev <ignatev.my@domkominvest.ru>
2022-02-26 16:57:12 +0700

    in process

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,12 @@ const path = require('path');
 
 const babelOptions = (preset) => {
     const opts = {
-        presets: ["@babel/preset-env"],
-        plugins: ["@babel/plugin-proposal-class-properties", "@babel/plugin-syntax-jsx"],
+        presets: ["@babel/preset-env", "@babel/preset-react"],
+        plugins: [
+            "@babel/plugin-proposal-class-properties",
+            "@babel/plugin-syntax-jsx",
+            "@babel/plugin-transform-runtime"
+        ],
     };
 
     if (preset) {
@@ -24,20 +28,44 @@ const jsLoaders = () => {
     return loaders;
 };
 
-module.exports = {
-    entry: './resources/js/app.js',
-    output: {
-        filename: 'app.js',
-        path: path.resolve(__dirname, 'public/js'),
-        clean: true,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: jsLoaders(),
+module.exports = (env, argv) => {
+    return {
+        mode: argv.mode === "production" ? "production" : "development",
+        entry: ['babel-polyfill', './resources/js/app.js'],
+        devtool: 'inline-source-map',
+        output: {
+            filename: 'app.js',
+            path: path.resolve(__dirname, 'public/js'),
+            clean: true,
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: jsLoaders(),
+                },
+                {
+                    test: /\.jsx$/,
+                    exclude: /node_modules/,
+                    use: [
+                        {
+                            loader: "babel-loader",
+                            options: babelOptions("@babel/preset-react"),
+                        },
+                    ],
+                },
+            ],
+        },
+        devServer: {
+            static: {
+                directory: path.join(__dirname, 'public'),
+                // publicPath: 'http://localhost:8080'
             },
-        ],
-    },
+            port: 8080,
+            // hot: 'only',
+            // liveReload: false,
+            // headers: { 'Access-Control-Allow-Origin': '*' }
+        },
+    }
 };
